7e56a498370a0675fb90f2a48c6b9783fb6de24a
Shivam Singh <shivamsinghgbpit@gmail.com>
2024-11-16 01:40:20 +0530

    Add storing log files logic

--- public/logger/Logger.js
+++ public/logger/Logger.js
@@ -1,8 +1,17 @@
 import url from 'url';
+import fs from 'fs/promises'
+import path from 'path';
 
 function logger(req, res, next) {
     let now = new Date();
-    console.log(`${now.toLocaleString()} - localhost:3000${req.originalUrl}`, req.method)
+    let log = `${now.toLocaleString()}-localhost:3000${req.originalUrl}-${req.method}\n`;
+    let __filePath = url.fileURLToPath(import.meta.url);
+    let __dirname = path.dirname(__filePath);
+    let logPath = path.join(__dirname, 'logs', `${now.getUTCDate()}_server_log_file.txt`);
+    fs.appendFile(logPath, log, 'utf-8').then((res) => {
+    }).catch((err) => {
+        console.log(err);
+    })
     next();
 }
 
